32fa4b37b7ffd3202d803a73025f1cf24d91b48f
Sanjeev Kumar <karthikjaiswal10111@gmail.com>
2025-06-28 21:37:47 +0530

    nothing changed

---
+++ practise.java
@@ -0,0 +1,52 @@
+public class practise {
+    // public static void main(String[] args) {
+    //     int num = 101;
+    //     binarytodec(num);
+    //     int[] arr = { 4, 6, 9, 2, 1, 4, 6 };
+    // }
+
+    // public static void binarytodec(int num) {
+    //     int binary = num;
+    //     int pow = 0;
+    //     int decnum = 0;
+    //     while (num > 0) {
+    //         int lastdigit = num % 10;
+    //         decnum = decnum + (lastdigit * (int) Math.pow(2, pow));
+    //         pow++;
+    //         num = num / 10;
+    //     }
+    //     System.out.println("decimal of " + binary + " = " + decnum);
+    // }
+
+    // public static void printarr(int arr[]) {
+//     for (int i : arr) {
+//         System.out.println(arr[i] + " ");
+//     }
+//     System.out.println();
+// }
+// public static void mergesort(int arr[], int si, int ei) {
+// }
+
+    static class info{
+        boolean isbst;
+        int size;
+        int min;
+        int max;
+        public info(boolean isbst,int size,int min,int max){
+            this.isbst = isbst;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+    }public int maxbst = 0;
+    public static info largestbst(Node root){
+        if(root == null){
+
+        }
+    }
+
+    // streak restore: another dummy comment for git commit
+}
+
+
+
