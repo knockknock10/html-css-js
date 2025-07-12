b22e7ee057dbe18cac37b6fe8427cf8a375dc25e
Sanjeev Kumar <karthikjaiswal10111@gmail.com>
2025-07-12 08:20:14 +0530

    BACKEND

---
+++ Backend/banana.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name:"banananana",
+    color : "Yellow"
+}
---
+++ Backend/index.js
@@ -0,0 +1,4 @@
+const apples = require("./apple");
+const bana = require("./banana");
+let f = [apples,bana];
+module.exports = f;
