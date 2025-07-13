d5a44efd98391f496eeddae3dc50bb7ddb92e803
Sanjeev Kumar <karthikjaiswal10111@gmail.com>
2025-07-13 20:36:23 +0530

    commit 6d5c0d7f3a1b2e4c8f9e1a2b3c4d5e6f7g8h9i0j

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;  
+
+const path = require("path");
+//app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/htmlejs"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/dice", (req, res) => {
+  let diceval = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", { num: diceval });
+});
+
+app.get("/ig/:username",(req,res)=>{
+  // let followers = ["bob","ravi","hari","john","Ela"];
+  let {username} = req.params;
+  const instadata = require("./data.json");
+  const data = instadata[username];
+  if(data){
+    res.render("instagram.ejs",{data : instadata[username]});
+  }else{
+    res.render("err.ejs");
+  }
+})
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});
