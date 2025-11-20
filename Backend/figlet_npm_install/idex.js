b34f90242d251a02f03900096beb345bcc48cb54
knock <116096166+knockknock10@users.noreply.github.com>
2025-11-20 16:10:09 +0530

    Delete Backend/figlet_npm_install directory

--- Backend/figlet_npm_install/idex.js
+++
@@ -1,9 +0,0 @@
-const figlet = require("figlet");
-figlet("Sanjeev",function(err,data){
-    if(err){
-        console.log("something went wrong");
-        console.log(err);
-        return;
-    }
-    console.log(data);
-})
